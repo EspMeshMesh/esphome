d69281cdc966b5fb7e6a81073f1ffdc8f1ddd786
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2024-12-24 09:34:00 +0000

    output buffer dynamic allocation

--- esphome/components/meshmesh/memringbuffer.cpp
+++ esphome/components/meshmesh/memringbuffer.cpp
@@ -90,10 +90,6 @@ uint16_t MemRingBuffer::viewData(uint8_t *data, uint16_t size) const {
     size = filledSpace();
   uint16_t readed = size;
 
-  int16_t ss = (mTail - mBuffer) - (mHead - mBuffer);
-  if (ss < 0)
-    ss = mAllocatedSize + ss;
-
   uint16_t contfillspace = continousFilledSpace();
   if (contfillspace > size)
     contfillspace = size;
@@ -101,6 +97,37 @@ uint16_t MemRingBuffer::viewData(uint8_t *data, uint16_t size) const {
 
   size -= contfillspace;
   if (size > 0) {
+    memcpy(data + contfillspace, mBuffer, size);
+  }
+
+  return readed;
+}
+
+uint16_t MemRingBuffer::viewData2(uint8_t *data, uint16_t size, uint16_t offset) const {
+  // If offset is greater than the filled space, return 0
+  if (offset > filledSpace()) {
+    return 0;
+  } else if (offset + size > filledSpace()) {
+    // If the offset + size is greater than the filled space, reduce the size to the available data for reading
+    size = filledSpace() - offset;
+  }
+
+  uint8_t readed = size;
+  // Recalculate offset to be relative to the buffer start
+  offset = ((mHead - mBuffer) + offset) % mAllocatedSize;
+  // Calculate the virtual head of the buffer
+  uint8_t *myHead = mBuffer + offset;
+  // Return the number of bytes that can be read from the current head to the tail of the buffer without wrapping.
+  uint16_t contfillspace = continousFilledSpace2(myHead, mTail);
+  // If the number of bytes that can be read is greater than the requested size, read the requested size
+  if (contfillspace > size)
+    contfillspace = size;
+  // Copy the data from the virtual head to the requested data
+  memcpy(data, myHead, contfillspace);
+  // Note: contfillspace is always less than or equal to size, we can safely subtract it from size.
+  size -= contfillspace;
+  if (size > 0) {
+    // If there is more data to read, copy the data from the buffer start to the requested data
     memcpy(data + contfillspace, mBuffer, size);
   }
 
